65b445f39c3c2cc24bbc3c9c8c2caba485452d9e
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-28 17:52:02 +0530

    stack created

---
+++ src/com/bridgelabz/Stack.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Stack <T> {
+
+    LinkedList<T> linkedList;
+
+    public Stack() {
+        this.linkedList = new LinkedList<>();
+    }
+
+
+    public void push(T key) {
+        linkedList.push(key);
+    }
+
+
+    public void print() {
+        linkedList.print();
+    }
+}
